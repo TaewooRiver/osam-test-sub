e11cfe8341294ca95c1c64cd6ea8260fea681aff
= <=>
2022-09-13 12:09:34 +0000

    Modified App.js to show each movie div

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,30 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([])
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Don't Edit <code>src/App.js</code> and save to reload.
-        </p>
-				
-				<p>
-					practice merging confilcts
-				</p>
-		  	<p>
-				{ console.log("Update from osam-test(not sub!)") }
-		  	</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => <div key={movie.id}>
+        <img src={movie.medium_cover_image}></img>
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map(g => <li key={g}>{g}</li>)}
+        </ul>
+      </div>)}</div>}
     </div>
   );
 }
