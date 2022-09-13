aa80bcb67b4327f1c47c24b25bbb23fee1097cad
= <=>
2022-09-13 06:59:20 +0000

    test1-deleted everything and wrote something

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,30 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Don't Edit <code>src/App.js</code> and save to reload.
-        </p>
-				
-				<p>
-					practice merging confilcts
-				</p>
-		  	<p>
-				{ console.log("Update from osam-test(not sub!)") }
-		  	</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Testing...
     </div>
   );
 }
